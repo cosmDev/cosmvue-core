b0d0997b78753d1e1b86859f850510699acf31b6
atmoner <contact.atmoner@gmail.com>
2025-03-29 18:10:44 +0100

    Update version

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-
 import LoginWallet from "./components/LoginWallet.vue";
+import WalletInfo from "./components/WalletInfo.vue"; 
 
 export default {
     install: (app, options) => {
       app.component("login-wallet", LoginWallet);
+      app.component("wallet-info", WalletInfo);
     }
 }
