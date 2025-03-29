9a2cfa6145008176dff37ba71ba163bf139c08b7
atmoner <contact.atmoner@gmail.com>
2025-03-29 18:24:28 +0100

    Update version

--- components/WalletInfo.vue
+++ components/WalletInfo.vue
@@ -1,28 +1,35 @@
 <template> 
-<table> 
-  <tbody>
-    <tr>
-      <th scope="row">Available amount</th>
-      <td>4,880</td> 
-    </tr>
-    <tr>
-      <th scope="row">Delegated amount</th>
-      <td>12,104</td>
-    </tr>
-    <tr>
-      <th scope="row">Undelegated</th>
-      <td>12,742</td>
-    </tr>
-    <tr>
-      <th scope="row">Reward</th>
-      <td>6,779</td> 
-    </tr>
-  </tbody>
-  <tfoot>
-    <tr>
-      <th scope="row">Average</th>
-      <td>9,126</td> 
-    </tr>
-  </tfoot>
-</table>
+  <div class="grid">
+    <div>
+      <article>
+        <header>Wallet Info</header>
+        <table> 
+          <tbody>
+            <tr>
+              <th scope="row">Available amount</th>
+              <td>4,880</td> 
+            </tr>
+            <tr>
+              <th scope="row">Delegated amount</th>
+              <td>12,104</td>
+            </tr>
+            <tr>
+              <th scope="row">Undelegated</th>
+              <td>12,742</td>
+            </tr>
+            <tr>
+              <th scope="row">Reward</th>
+              <td>6,779</td> 
+            </tr>
+          </tbody>
+          <tfoot>
+            <tr>
+              <th scope="row">Average</th>
+              <td>9,126</td> 
+            </tr>
+          </tfoot>
+        </table>
+      </article>  
+    </div> 
+  </div>
 </template>
