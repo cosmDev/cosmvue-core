c212f336eb4a3e238a62ff22b2f3569b452127fe
atmoner <contact.atmoner@gmail.com>
2025-03-29 16:30:29 +0100

    First commit

---
+++ components/LoginWallet.vue
@@ -0,0 +1,22 @@
+<template> 
+    <div class="flex two">
+ 
+    <article class="card">
+    <header>
+        <h3><slot name="title" /></h3>
+    </header>
+    <footer>
+        <button><slot name="title-btn" /></button>
+    </footer>
+    </article>
+    </div>
+
+    <div class="flex demo">
+  <div><span>1</span></div>
+  <div><span>2</span></div>
+  <div><span>3</span></div>
+  <div><span>4</span></div>
+  <div><span>5</span></div>
+  <div><span>6</span></div>
+</div>
+  </template>
---
+++ index.js
@@ -0,0 +1,18 @@
+
+import LoginWallet from "./components/LoginWallet.vue";
+
+
+export default {
+    install: (app, options) => {
+      app.component("login-wallet", LoginWallet);
+
+      // inject a globally available $translate() method
+      app.config.globalProperties.$translate = (key) => {
+        // retrieve a nested property in `options`
+        // using `key` as the path
+        return key.split('.').reduce((o, i) => {
+          if (o) return o[i]
+        }, options)
+      }
+    }
+}
